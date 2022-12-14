255668cf7a672cbdebc8c25da1e1308b0db1ece6
Pranavesh Mara <pranavesh.mara@ratnaglobaltech.com>
2022-12-14 17:15:04 +0530

    changes to be added in  One_to_One class

--- src/main/java/com/example/demo/OneToOneApplication.java
+++ src/main/java/com/example/demo/OneToOneApplication.java
@@ -2,10 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OneToOneApplication {
+public class OneToOneApplication extends SpringBootServletInitializer{
 
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(OneToOneApplication.class);
+	    }
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OneToOneApplication.class, args);
 	}
